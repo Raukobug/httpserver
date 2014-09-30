334d2b6d9dd4817b9292d59b34b43b3eea913bf2
Raukobug <rauko1@gmail.com>
2014-09-30 09:22:35 +0200

    HtmlGenerator and StatusHandler class created.
    Useing them to handel html and errors.

--- httpserver/HttpServer.cs
+++ httpserver/HttpServer.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
@@ -13,21 +12,6 @@ namespace httpserver
         public static readonly int DefaultPort = 8080;
         private static readonly string RootCatalog = Directory.GetCurrentDirectory();
         private const string Version = "HTTP/1.0 ";
-
-        //Makeing the error message display on the browser
-        private const string HtmlStart = @"
-<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
-<html xmlns='http://www.w3.org/1999/xhtml'>
-<head>
-<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-<title>Error</title>
-</head>
-<body>
-";
-        private const string HtmlEnd = @"
-</body>
-</html>
-";
 
         EventLog myLog = new EventLog();
         public void StartServer()
@@ -48,30 +32,30 @@ namespace httpserver
                 Stream ns = new NetworkStream(connectionSocket);
                 var sr = new StreamReader(ns, Encoding.UTF8);
                 var sw = new StreamWriter(ns) { AutoFlush = true };
-                myLog.WriteEntry("Client request.", EventLogEntryType.Information, 2);
                 //formates the input form the stream to a usefull formate
                 string srtext = sr.ReadLine();
+                myLog.WriteEntry("Client request: " + srtext, EventLogEntryType.Information, 2);
 
                 if (srtext != null)
                 {
                     string[] words = srtext.Split(' ');
                     string getFile = words[1].Replace("/", "\\");
                     string path = RootCatalog + getFile;
-
+                    if (getFile == "\\")
+                    {
+                        path = RootCatalog + "\\index.html";
+                    }
+                    var sh = new StatusHandler(srtext,path);
                     string text = "";
-                    string serverrespons = "";
-
+                    string consoleText = sh.ServerRespons();
+                    var hg = new HtmlGenerator(sh.ServerRespons(), Version);
                     //If the file exists retun the file else return a error 404 Not Found
                     try
                     {
                         //If no get is set take look for the index.html.
-                        if (getFile == "\\")
-                        {
-                            path = RootCatalog + "\\index.html";
-                        }
                         if (File.Exists(path))
                         {
-                            serverrespons = Version + ErrorHandler(200);
+                            consoleText = Version + sh.ServerRespons();
 
                             //Opens the file from the path and take each byte in the file to a new string.
                             using (FileStream fs = File.OpenRead(path))
@@ -86,17 +70,18 @@ namespace httpserver
                         }
                         else
                         {
-                            serverrespons = Version + ErrorHandler(404);
-                            text = HtmlStart + Version + ErrorHandler(404) + HtmlEnd;
+                            consoleText = Version + sh.ServerRespons();
+                            text = hg.GetSite();
                         }
+
                     }
                     finally
                     {
                         sw.Write(@text);
-                        Console.Write(serverrespons + "\n");
+                        Console.Write(consoleText + "\n");
                         ns.Close();
                         connectionSocket.Close();
-                        myLog.WriteEntry("Server respons.", EventLogEntryType.Information, 3);
+                        myLog.WriteEntry("Server respons: " + sh.ServerRespons(), EventLogEntryType.Information, 3);
                     }
                 }
             }
@@ -104,11 +89,6 @@ namespace httpserver
             myLog.WriteEntry("Server shutdown.", EventLogEntryType.Information, 4);
         }
 
-        //Handels status-codes.
-        public string ErrorHandler(int id)
-        {
-            var myList = new List<string> { "200 OK", "201 Created", "202 Accepted", "204 No Content", "301 Moved Permanently", "302 Moved Temporarily", "304 Not Modified", "400 Bad Request", "401 Unauthorized", "403 Forbidden", "404 Not Found", "500 Internal Server Error", "501 Not Implemented", "502 Bad Gateway", "503 Service Unavailable"};
-            return myList.Find(x => x.Contains(Convert.ToString(id)));
-        }
+
     }
 }
---
+++ httpserver/StatusHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace httpserver
+{
+    //This class is used to handel any error there might happen.
+    class StatusHandler
+    {
+        private readonly List<string> _myList = new List<string> {"201 Created", "202 Accepted", "204 No Content", "301 Moved Permanently", "302 Moved Temporarily", "304 Not Modified", "400 Bad Request", "401 Unauthorized", "403 Forbidden", "404 Not Found", "500 Internal Server Error", "501 Not Implemented", "502 Bad Gateway", "503 Service Unavailable" };
+        private string _errorMessege = "200 OK";
+        private string _clientRequest;
+        private readonly string _path;
+        public StatusHandler(string clientRequest, string path)
+        {
+            _clientRequest = clientRequest;
+            _path = path;
+            StatusLookUp();
+        }
+
+        //Sets the status code if something is wrong
+        private void StatusLookUp()
+        {
+            if (!File.Exists(_path))
+            {
+                _errorMessege = ErrorHandler(404);
+            }
+        }
+
+        //Looks up status-codes.
+        public string ErrorHandler(int id)
+        {
+            return _myList.Find(x => x.Contains(Convert.ToString(id)));
+        }
+
+        //Return status-code.
+        public string ServerRespons()
+        {
+            return _errorMessege;
+        }
+    }
+}
